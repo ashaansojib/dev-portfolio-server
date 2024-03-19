f16a53b50f6b50fcb03daffb5385e54563573e91
ashaansojib <ashaduzzamansojib67@gmail.com>
2024-03-19 21:32:03 +0600

    complete search query

--- routes/projects.js
+++ routes/projects.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.route("/").get(getProjects);
 router.route("/").post(createProject);
-router.route("/category").get(getProjectByCat);
+router.route("/search").get(getProjectByCat);
 router.route("/:id").get(getProject);
 router.route("/:id").delete(removeProject);
 module.exports = router;
