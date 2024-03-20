887529504e61275f15a0c3da21f457c1cf82e23b
ashaansojib <ashaduzzamansojib67@gmail.com>
2024-03-20 12:13:44 +0600

    async code different

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,64 +1,49 @@
 const devProjects = require("../models/Projects");
+const asyncHandler = require("../middleware/async");
 // @desc    = fetch all projects data
 // @route   = /api/projects
 // access   = public
-exports.getProjects = async (req, res, next) => {
-  try {
-    const projects = await devProjects.find();
-    res.status(200).json({ success: true, data: projects });
-  } catch (err) {
-    next(err);
-  }
-};
+exports.getProjects = asyncHandler(async (req, res, next) => {
+  const projects = await devProjects.find();
+  res.status(200).json({ success: true, data: projects });
+});
 
 // @desc    = add a project
 // @route   = /api/projects
 // @access  = privet
-exports.createProject = async (req, res, next) => {
-  try {
-    const projectData = req.body;
-    const results = await devProjects.create(projectData);
-    res.status(201).json({ success: true, data: results });
-  } catch (err) {
-    next(err);
-  }
-};
+exports.createProject = asyncHandler(async (req, res, next) => {
+  const projectData = req.body;
+  const results = await devProjects.create(projectData);
+  res.status(201).json({ success: true, data: results });
+});
 
 // @desc    = single project find
 // @route   = /api/projects/:id
 // @access  = public
-exports.getProject = async (req, res, next) => {
-  try {
-    const id = req.params.id;
-    const query = await devProjects.findById(id);
-    res.status(200).json({ success: true, data: query });
-  } catch (err) {
-    next(err);
-  }
-};
+exports.getProject = asyncHandler(async (req, res, next) => {
+  const id = req.params.id;
+  const query = await devProjects.findById(id);
+  res.status(200).json({ success: true, data: query });
+});
 
 // @desc    = filter by category
 // @@route  = /api/projects/?category=MERN-stack
 // @access  = public
-exports.getProjectByCat = async (req, res, next) => {
-  try {
-    const category = req.query;
-    const results = await devProjects.find(category);
-    res.status(200).json({ success: true, data: results })
-  } catch (err) {
-    next(err)
-  }
-}
+exports.getProjectByCat = asyncHandler(async (req, res, next) => {
+
+  const category = req.query;
+  const results = await devProjects.find(category);
+  res.status(200).json({ success: true, data: results })
+
+});
 
 // @desc    = delete a projects
 // @route   = /api/projects/:id
 // @access  = privet
-exports.removeProject = async (req, res, next) => {
-  try {
-    const id = req.params.id;
-    const query = await devProjects.deleteOne({ _id: id });
-    res.status(200).json({ success: true, data: query });
-  } catch (err) {
-    next(err);
-  }
-};
+exports.removeProject = asyncHandler(async (req, res, next) => {
+
+  const id = req.params.id;
+  const query = await devProjects.deleteOne({ _id: id });
+  res.status(200).json({ success: true, data: query });
+
+});
